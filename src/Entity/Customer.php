576533c6911272606e1aa902273a02ca311296ea
davidcai2011 <100039017+davidcai2011@users.noreply.github.com>
2022-06-14 11:14:08 +0200

    Add api, easy admin all works

---
+++ src/Entity/Customer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CustomerRepository::class)]
+#[ApiResource]
+class Customer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $CustomerName;
+
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    private $Company;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getCustomerName(): ?string
+    {
+        return $this->CustomerName;
+    }
+
+    public function setCustomerName(string $CustomerName): self
+    {
+        $this->CustomerName = $CustomerName;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->Company;
+    }
+
+    public function setCompany(?string $Company): self
+    {
+        $this->Company = $Company;
+
+        return $this;
+    }
+}
